ad331c8d7b61cadca12e8e211d4cc9ac8eb60600
Denis Lom <lom.denis@seznam.cz>
2024-10-29 01:26:13 +0100

    Simple Bot Hyperskill

--- Topics/If expression/Positive, Negative or Zero/src/Main.kt
+++ Topics/If expression/Positive, Negative or Zero/src/Main.kt
@@ -1,3 +1,9 @@
 fun main() {
     // write your code here
+    val number: Int = readln().toInt()
+    when {
+        number < 0 -> println("negative")
+        number > 0 -> println("positive")
+        number == 0 -> println("zero")
+    }
 }
