862c888f9f8d8c498daae7c61a2a0d57408a730f
Denis Lom <lom.denis@seznam.cz>
2024-10-29 01:23:01 +0100

    Simple Bot Hyperskill

--- Topics/If expression/Number dividers/src/Main.kt
+++ Topics/If expression/Number dividers/src/Main.kt
@@ -1,3 +1,8 @@
 fun main() {
     // put your code here
+    val number: Int = readln().toInt()
+    if (number % 2 == 0) println("Divided by 2")
+    if (number % 3 == 0) println("Divided by 3")
+    if (number % 5 == 0) println("Divided by 5")
+    if (number % 6 == 0) println("Divided by 6")
 }
