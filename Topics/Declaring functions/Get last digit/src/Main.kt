b70f0a28b0debaff0ebdfcba1ce4b744adb3210a
Denis Lom <lom.denis@seznam.cz>
2024-11-03 23:43:29 +0100

    Declaring functions: getLastDigit()

---
+++ Topics/Declaring functions/Get last digit/src/Main.kt
@@ -0,0 +1,12 @@
+// write your code here
+fun getLastDigit(digit: Int): Int {
+    if (digit < 0)
+        return (-digit % 10)
+    else
+        return (digit % 10)
+}
+/* Do not change code below */
+fun main() {
+    val a = readLine()!!.toInt()
+    println(getLastDigit(a))
+}
